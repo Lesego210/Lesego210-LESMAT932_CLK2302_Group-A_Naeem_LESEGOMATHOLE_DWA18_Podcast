8d64b7099aad94c10b5c15d7f709f6499334bd3e
Lesego210 <127973618+Lesego210@users.noreply.github.com>
2024-08-01 20:25:20 +0200

    Login.jsx

--- src/Login.jsx
+++ src/Login.jsx
@@ -1,33 +1,56 @@
-import { createClient } from '@supabase/supabase-js';
-import { Auth } from '@supabase/auth-ui-react';
-import { ThemeSupa } from '@supabase/auth-ui-shared';
+import { createClient } from '@supabase/supabase-js'
+import { Auth } from '@supabase/auth-ui-react'
+import { ThemeSupa } from '@supabase/auth-ui-shared'
 import { useNavigate } from 'react-router-dom';
+import { useCallback, useEffect } from 'react';
 
-const supabaseUrl = 'https://mkayoundsyjpvbejcvnh.supabase.co';
+
+const  supabaseUrl = 'https://mkayoundsyjpvbejcvnh.supabase.co';
 const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im1rYXlvdW5kc3lqcHZiZWpjdm5oIiwicm9sZSI6ImFub24iLCJpYXQiOjE2OTk1MzkwNTgsImV4cCI6MjAxNTExNTA1OH0.RVVkcFxkfycYdSwoAfkONPmaDfgzAd-9DVZUHWn13ps'; // Replace with your actual Supabase API key
 
-const supabase = createClient(supabaseUrl, supabaseKey);
+const supabase = createClient(
+    supabaseUrl,
+    supabaseKey
+);
 
 const LoginPage = () => {
   const navigate = useNavigate();
 
-  const handleLogin = () => {
-    // Perform login logic
-    navigate('/podcasts'); // Use navigate to redirect after login
-  };
+  const handleAuthStateChange = useCallback(
+    async (event) => {
+      if (event == 'SIGNED_OUT') {
+        navigate('/');
+      } else if (event == 'SIGNED_IN') {
+        navigate('/podcasts');
+      }
+    },
+    [navigate]
+  );
+
+  useEffect(() => {
+    // Set up the onAuthStateChange listener with the callback function
+    supabase.auth.onAuthStateChange(handleAuthStateChange);
+
+  
+
+    // Cleanup when the component unmounts
+    return () => {
+      // Perform any necessary cleanup here
+    };
+  }, [handleAuthStateChange]);
 
   return (
     <div>
-      <h2>Login Page</h2>
+
+
+      <h2 className='topHeader'>
+        Podcast
+      </h2>
       <Auth
-        supabaseClient={supabase}
-        appearance={{ theme: ThemeSupa }}
-        onAuthStateChange={(event, session) => {
-          if (session) {
-            handleLogin();
-          }
-        }} // Call handleLogin on successful login
-      />
+    supabaseClient={supabase}
+    
+    appearance={{ theme: ThemeSupa }}
+  />
     </div>
   );
 };
